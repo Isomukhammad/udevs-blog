2789f154b88bab5cec118cfbee536b421b151d06
Isomukhammad <s.isomuxammad@gmail.com>
2022-08-22 17:49:21 +0500

    Fixing enhancer

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,11 @@ import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 import thunk from 'redux-thunk';
 
-const middleWares = [process.env.NODE_ENV !== 'development' && logger, thunk].filter(
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(
     Boolean
 )
 
-const composeEnhansers = (process.env.NODE_ENV !== 'development' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; 
+const composeEnhansers = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; 
 
 const composedEnhansers = composeEnhansers(applyMiddleware(...middleWares));
 
