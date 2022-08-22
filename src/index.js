563cfd7d17084aa23ed055432bdd4ca0638d0d85
Isomukhammad <s.isomuxammad@gmail.com>
2022-08-22 17:00:39 +0500

    Fixing router-dom

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ import './index.css';
 import App from './App';
 import {store} from './store/store'
 
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store = {store}>
-    <BrowserRouter>
+    <HashRouter>
       <App/>
-    </BrowserRouter> 
+    </HashRouter> 
   </Provider>   
 );
 
