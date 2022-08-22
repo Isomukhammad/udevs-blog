86bbabcc6dcf5cf46321a0c531f58af0eb030e4c
Isomukhammad <s.isomuxammad@gmail.com>
2022-08-22 17:18:55 +0500

    Switching enhancer from development to production

--- src/store/store.js
+++ src/store/store.js
@@ -7,7 +7,7 @@ const middleWares = [process.env.NODE_ENV !== 'development' && logger, thunk].fi
     Boolean
 )
 
-const composeEnhansers = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; 
+const composeEnhansers = (process.env.NODE_ENV !== 'development' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose; 
 
 const composedEnhansers = composeEnhansers(applyMiddleware(...middleWares));
 
